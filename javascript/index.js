778f28dd9090dadcd1dfd2c48ce58293bd2a1548
smayersp <134955190+smayersp@users.noreply.github.com>
2024-10-03 13:39:03 -0400

    fixed small element

--- javascript/index.js
+++ javascript/index.js
@@ -28,15 +28,17 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector(".cities");
-  citiesElement.innerHTML = `<div class="city">
+  citiesElement.innerHTML = `
+  <div class="city">
           <div>
             <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
-          )} <small>${cityTime.format("A")}</div>
-        </div>`;
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>
+        `;
 }
 
 updateTime();
